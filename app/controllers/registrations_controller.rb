6d659e93bf4ac17fcd785db78528f990ae68886a
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-24 13:47:44 -0800

    Refactoring

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   def new
     @worker = Worker.new
-  
+
     if user_signed_in?
-      sign_out :current_user
+      sign_out current_user
       flash[:notice] = "You have been signed out of your user account."
     end
   end
