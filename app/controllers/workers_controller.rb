08f1ae64b72cf3355b02054c472547569f58ffc6
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-02-23 20:11:19 -0800

    Fix worker path, remove routes

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,20 +1,15 @@
 class WorkersController < ApplicationController
+  before_action :authenticate_worker!, only: [:show]
   def show
     @worker = current_worker
-    render :show
   end
 
   def new
+    @worker = Worker.new
     # current_worker refers to a worker account currently logged in. current_user refers to a user account currently logged in.
     if current_worker
       redirect_to worker_path(current_worker)
       flash[:notice] = "You're already logged into a worker account!"
-    elsif current_user
-      # need to make sure users signing up to be workers are signed out of their user account first. -Mr. Fix-It
-    sign_out :user
-    redirect_to new_worker_registration_path
-    else
-      redirect_to new_worker_registration_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   root 'landing#index'
 
   resources :jobs
-  resources :workers
+
 end
